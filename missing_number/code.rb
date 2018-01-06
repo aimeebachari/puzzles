cd3b57ed24dba12fab746a9368814650e3480f9a
Aimee Bachari <aimeebachari@gmail.com>
2018-01-06 11:50:22 -0600

    missing number challenge

---
+++ missing_number/code.rb
@@ -0,0 +1,11 @@
+def missing_number(array)
+  array.sort!
+  return 1 if array[0] != 1
+  array.each_with_index do |num, i|
+    return num + 1 if num + 1 != array[i + 1]
+  end
+end
+
+array = [1, 8, 3, 4, 6, 2, 5, 9]
+
+puts missing_number(array)
